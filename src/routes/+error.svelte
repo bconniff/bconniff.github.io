3b6b240f0ce27a16aaf13c823b0523cfd5b98a84
Brendan <brendan@slack.azcl.lan>
2024-12-22 20:38:38 -0500

    Add error handler / test route

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<!-- vim:set ft=html sw=2 sts=2 ts=2: -->
+
+<script>
+  import { page } from '$app/stores';
+</script>
+
+<svelte:head>
+  <title>Brendan Conniff &mdash; {$page.error.message}</title>
+  <meta name="description" content={$page.error.message} />
+</svelte:head>
+
+<h1>{$page.status}: {$page.error.message}</h1>
